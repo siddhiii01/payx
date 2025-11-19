ae9696f4b08fefb364a5b8c202403f18ad896e95
siddhiii01 <siddhiigupta15@gmail.com>
2025-11-19 20:17:10 +0530

    Prisma Stup version 6

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import type {Request, Response, NextFunction} from "express";
 import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client"; // Default location
 
 dotenv.config();
 const app = express();
@@ -14,7 +15,28 @@ app.use((req: Request,res:Response, next: NextFunction) => {
     console.log(`req.bpdy: ${JSON.stringify(req.body)}`);
     console.log(`req.url: ${req.originalUrl}`);
     next();
-})
+});
+
+
+// Initialize Prisma with adapter
+const prisma = new PrismaClient({});
+
+
+
+
+async function testConnection() {
+  try {
+    // Test database connection
+    await prisma.$connect();
+    console.log('Prisma connected successfully!');
+  } catch (error) {
+    console.error('Prisma connection failed:', error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+testConnection();
 
 app.get('/test', (req: Request, res: Response) => {
     console.log("Test Route");
