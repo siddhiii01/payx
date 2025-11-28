0507627ea1427121900a97571ae2934f17e12074
siddhiii01 <siddhiigupta15@gmail.com>
2025-11-28 18:13:52 +0530

    Zod validation

---
+++ backend/src/validations/auth.schema.ts
@@ -0,0 +1,18 @@
+import {z} from "zod";
+
+const login = z.object({
+    email: z.string().trim().min(1, "Email is required").email({message: "Invalid Email format"}),
+    password: z.string().min(1, "Password is required")
+
+});
+
+const register = z.object({
+    username: z.string(),
+    email: z.string().trim().min(1, "Email is required").email({ message: "Invalid email format" }),
+    password: z.string()
+});
+
+export const  authSchema = {
+    login,
+    register
+}
